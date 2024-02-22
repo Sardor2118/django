73837d4a0f3a097dbc375beb5d5c4bbeef80f936
Sardor2118 <mo3rus@gmail.com>
2024-02-22 19:13:39 +0500

    2 urok django

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from products.models import ProductModel
 
-# Create your views here.
+def home_page(request):
+    products = ProductModel.objects.all()
+    context = {'products': products}
+    return render(request, template_name='index.html', context=context)
+
+def products_page(request):
+    products = ProductModel.objects.all()
+    context = {'products': products}
+    return render(request, template_name='products.html', context=context)
