e2088a36b4afd8b4a4209c811bd9ad4c57ac1367
Sardor2118 <mo3rus@gmail.com>
2024-02-24 19:15:17 +0500

    2 urok django

--- django48/urls.py
+++ django48/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from products.views import home_page, products_page
+from products.views import home_page, products_page, MyLoginView, logout_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+class MyLogoutView:
+    pass
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
+    path('', home_page, name='home_page'),
     path('products/', products_page),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-from products.models import ProductModel
+from django.shortcuts import render, redirect
+from products.models import ProductModel, CategoryModel
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
 
 def home_page(request):
     products = ProductModel.objects.all()
-    context = {'products': products}
+    categories = CategoryModel.objects.all()
+    context = {'products': products, 'categories': categories}
     return render(request, template_name='index.html', context=context)
 
 def products_page(request):
     products = ProductModel.objects.all()
     context = {'products': products}
     return render(request, template_name='products.html', context=context)
+
+# login code
+class MyLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home_page')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home_page')
